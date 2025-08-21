ef4e4305c45b802a93c58fe9ab4ecde1e4d2ec1d
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-08-21 14:59:38 +0300

    Init

---
+++ config/config.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const BASE_URL = "https://localhost:4000";
+export const CLIENT_URL = "https://localhost:3000";
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+});
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        default: ["16px", { lineHeight: "22px" }],
+      },
+      colors: {
+        white: "#FFFFFF",
+        black: "#0D0D0D",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)", "sans-serif"],
+        vibes: ["Great Vibes", "cursive"],
+      },
+    },
+    screens: {
+      mini: "520px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "15px",
+        sm: "20px",
+        lg: "40px",
+        xl: "60px",
+      },
+    },
+  },
+  plugins: [],
+};
