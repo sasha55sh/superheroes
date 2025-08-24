d021c244a4ffbfbffb6beb83fe58a761eb82c885
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-08-24 17:49:10 +0300

    features: create 404 and 500 error pages

---
+++ app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+import ServerErrorSection from "@/app/sections/error-pages/ServerErrorSection";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <ServerErrorSection />
+    </>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import NotFoundSection from "@/app/sections/error-pages/NotFoundSection";
+
+export default function NotFound() {
+  return (
+    <>
+      <NotFoundSection />
+    </>
+  );
+}
